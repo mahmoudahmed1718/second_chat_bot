b60134c0327929ae64dca74f025f397cd54ea751
mahmoud ahmed <mahmoudahmedhsn@gmail>
2026-02-06 00:56:45 +0200

    add file cubit and state

---
+++ lib/features/home/presentation/manger/cubit/home_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:second_chat_bot/features/home/domain/entites/gemini_message_entity.dart';
+import 'package:second_chat_bot/features/home/domain/repo/get_gemine_reponse_repo.dart';
+
+part 'home_state.dart';
+
+class HomeCubit extends Cubit<HomeState> {
+  HomeCubit({required this.getGemineReponseRepo}) : super(HomeInitial());
+  final GetGemineReponseRepo getGemineReponseRepo;
+  Future<void> getGemineReponse({required String message}) async {
+    emit(HomeLoading());
+    final result = await getGemineReponseRepo.getGemineReponse(
+      message: message,
+    );
+    result.fold(
+      (error) => emit(HomeError(message: error.errorModel.message.toString())),
+      (success) => emit(HomeLoaded(messages: [success])),
+    );
+  }
+}
